34567297f7899ff473c2794164f083a73de1507d
Dave Blackburn <31860686+dwbcodes@users.noreply.github.com>
2026-06-16 22:48:04 -0700

    test

--- open-next.config.ts
+++ open-next.config.ts
@@ -3,7 +3,6 @@ import { defineCloudflareConfig } from "@opennextjs/cloudflare";
 // import r2IncrementalCache from "@opennextjs/cloudflare/overrides/incremental-cache/r2-incremental-cache";
 
 export default defineCloudflareConfig({
-	buildCommand: "npm run next:build",
 	// For best results consider enabling R2 caching
 	// See https://opennext.js.org/cloudflare/caching for more details
 	// incrementalCache: r2IncrementalCache
